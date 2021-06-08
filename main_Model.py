7d9b1f6df37f9055b4b37881df463f47727c5ed0
Aoklove <61591025+Aoklove@users.noreply.github.com>
2021-06-08 22:23:41 +0800

    Create main_Model.py

---
+++ main_Model.py
@@ -0,0 +1,19 @@
+def main_Model(X_trian,Y_train,learning_rate=0.05,iterations=10000,num_layer=5,print_cost=False,*kwargs):
+  """
+  this function is the main model of the project
+  arguments:
+  X_train--> the properties of trian
+  Y_train--> the label of data
+  learning_rate--> learing_rate of model
+  iterations--> iteration times
+  print_cost--> print the cost(per 100 times)
+  num_layer--> the layer of nerwork
+  
+  return:
+  costs--> 
+  w1-->
+  b1-->
+  w2-->
+  ...
+  """
+  pass  # waiting for code
